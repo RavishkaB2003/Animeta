328131a3a637fdf18bee7993de67ba05919e372f
Ravishka Praveen <dmrpbandara@students.nsbm.ac.lk>
2026-09-23 13:47:19 +0530

    bug fix

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const TrendingCard = ({  index }) => {
+const TrendingCard = ({ anime,  index }) => {
   return (
     <li className="flex flex-row items-center relative group cursor-pointer shrink-0">
       {/* Giant Stylized Rank Number */}
